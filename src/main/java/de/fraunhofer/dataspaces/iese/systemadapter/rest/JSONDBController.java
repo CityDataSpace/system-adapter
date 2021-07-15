24b66522a881a9a85aa56212acd012aae9cc62f6
Ajdari <ajdari@iese.fhg.de>
2021-07-15 10:23:08 +0200

    added new changes

--- src/main/java/de/fraunhofer/dataspaces/iese/systemadapter/rest/JSONDBController.java
+++ src/main/java/de/fraunhofer/dataspaces/iese/systemadapter/rest/JSONDBController.java
@@ -3,16 +3,23 @@ package de.fraunhofer.dataspaces.iese.systemadapter.rest;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import de.fraunhofer.dataspaces.iese.systemadapter.entity.JSON;
 import de.fraunhofer.dataspaces.iese.systemadapter.service.JSONService;
 
 @RestController
-@RequestMapping("/storage")
+@RequestMapping("/storage/database")
 public class JSONDBController {
 	
 	JSONService service;
@@ -31,5 +38,26 @@ public class JSONDBController {
 	public JSON getById(@PathVariable int json_id) {
 		return service.findById(json_id);
 	}
+	
+	@PostMapping("/")
+	public JSON postNew(@RequestBody JSON json) {
+		
+		json.setId(0);
+		
+		service.save(json);
+		
+		return json;
+	}
+	
+	@PutMapping("/")
+	public JSON putNew(@RequestBody JSON json) {
+		service.save(json);
+		
+		return json;
+	}
 
+	@DeleteMapping("/")
+	public void delete(@RequestBody int id) {
+		service.deleteById(id);
+	}
 }
