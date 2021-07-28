7156a9be429e46277c37855afe12d8ded99ce1a4
Ajdari <ajdari@iese.fhg.de>
2021-07-28 16:43:22 +0200

    fixing master

--- src/main/java/de/fraunhofer/dataspaces/iese/systemadapter/repository/JsonRepository.java
+++
@@ -1,8 +0,0 @@
-package de.fraunhofer.dataspaces.iese.systemadapter.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import de.fraunhofer.dataspaces.iese.systemadapter.entity.Json;
-
-public interface JsonRepository extends JpaRepository<Json, Integer> {
-
-}
